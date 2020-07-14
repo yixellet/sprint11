56416bda48992c9f0570f63850d6ac79406c2565
yixellet <yixwellet@gmail.com>
2020-07-14 12:58:16 +0400

    исправил замечания - удалил лишние директории images и fonts, добавил в readme подробную инструкцию, исправил main в package.json, отредактировал отступы в конфигах

--- webpack.config.js
+++ webpack.config.js
@@ -7,86 +7,86 @@ const isDev = process.env.NODE_ENV === 'development';
 const webpack = require ('webpack');
 
 module.exports = {
-    entry: './src/script/script.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[chunkhash].js'
-    },
+  entry: './src/script/script.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: { loader: 'babel-loader' },
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
-                    'css-loader', 
-                    'postcss-loader'
-                ]
-            },
-            {
-                test: /\.(eot|ttf|woff|woff2)$/i,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: './fonts/[name].[ext]'
-                    }
-                },
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/i,
-                use: [
-                    'file-loader?name=./images/[name].[ext]&exModule=false',
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            esModule: false,
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65
-                            },
-                            optipng: {
-                                enabled: false
-                            },
-                            pngquant: {
-                                quality: [0.65, 0.90],
-                                speed: 4
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            webp: {
-                                quality: 75
-                            }
-                        }
-                    }
-                ]
-            }
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: { loader: 'babel-loader' },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+          'css-loader', 
+          'postcss-loader'
         ]
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
-        new MiniCssExtractPlugin({
-            filename: './index.[contenthash].css'
-        }),
-        new WebpackMd5Hash(),
-        new webpack.DefinePlugin({
-            'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-        }),
-        new OptimizeCssAssetsPlugin({
-            assetNameRegExp: /\.css$/g,
-            cssProcessor: require('cssnano'),
-            cssProcessorPluginOptions: {
-                preset: ['default'],
-            },
-            canPrint: true
-        })
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/i,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: './fonts/[name].[ext]'
+          }
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          'file-loader?name=./images/[name].[ext]&exModule=false',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              esModule: false,
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              optipng: {
+                enabled: false
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ]
+      }
     ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: './index.[contenthash].css'
+    }),
+    new WebpackMd5Hash(),
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default'],
+      },
+      canPrint: true
+    })
+  ]
 }
